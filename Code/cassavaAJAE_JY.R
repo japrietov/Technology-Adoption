507226e69cf0d28ec21a43e26c11c6dc82c32f4d
Jing Yi <jy348@cornell.edu>
2023-12-13 03:21:32 -0600

    update

--- Code/cassavaAJAE_JY.R
+++ Code/cassavaAJAE_JY.R
@@ -3,7 +3,7 @@ rm(list = ls())
 dir <- getwd()
 
 OutputDir <- paste(dir, '/Output', sep = '')
-requiredPackages = c('gee','PropCIs','RVAideMemoire','ggpubr', 'broom', 'modelsummary','dplyr')
+requiredPackages = c('gee','PropCIs','RVAideMemoire','ggpubr', 'broom', 'modelsummary','dplyr','oaxaca')
 for(p in requiredPackages){
   if(!require(p,character.only = TRUE)) install.packages(p)
   library(p,character.only = TRUE)
@@ -45,7 +45,7 @@ results$y
 plot(results, components = c('endowments','coefficients'))
 
 
-dfSelfMag <- read.csv(paste0(OutputDir,'/dfSefMag.csv'))
+dfSelfMag <- read.csv(paste0(OutputDir,'/dfSefMag.csv'))=
 
 results <- oaxaca(
   formula = selfMagIcv ~ G_Coop + G_Credit + G_Ext_Only + G_CellValue + 
@@ -58,3 +58,48 @@ results <- oaxaca(
   R = 100
 )
 
+
+
+
+
+dfSelfMag <- read.csv("D:/Dropbox/BoxOld/FEDSshare/MasterGithub/CornellGitHub/DataSynthesisForCropVariety/Code/DataSynthesis/MeasureError/Gender/MeasureError/AJAE_2023/Technology-Adoption/Output/dfSefMag.csv")
+results <- oaxaca(
+  formula = selfMagIcv ~ G_Coop + G_Credit + G_Ext_Only + G_CellValue + 
+    Fertilizer + Consump.perc + Zone1 + Zone3 + 
+    Zone4 + Age + Education + `Household.size` + 
+    Main.occupation..Salaried.employment | A02_Sex | Zone1 + Zone3 + Zone4,
+  data = dfSelfMag,
+  reg.fun = glm,
+  family = binomial(link = "probit"),
+  R = 100
+)
+
+
+dfHead <- read.csv("D:/Dropbox/BoxOld/FEDSshare/MasterGithub/CornellGitHub/DataSynthesisForCropVariety/Code/DataSynthesis/MeasureError/Gender/MeasureError/AJAE_2023/Technology-Adoption/Output/head.csv")
+results <- oaxaca(
+  formula = DNA_planted  ~ Coop_Group_Member+Credit_Access+Extension_Access+CellAssValThd
+  +J01_hhd_apply_fert+J05_consumption_perc+ ZoneID_1 + ZoneID_3 + 
+    ZoneID_4 + A03_Age + A05_Edu + Household_Size + 
+    A07_1_Main_occupatn_Salaried_employment | A02_Sex | ZoneID_1 + ZoneID_3 + 
+    ZoneID_4,
+  data = dfHead,
+  reg.fun = glm,
+  family = binomial(link = "probit"),
+  R = 100
+)
+
+resultDNA['threefold']
+print(results)
+summary(result1)
+
+
+
+
+df <- read.csv("D:/Dropbox/BoxOld/FEDSshare/MasterGithub/CornellGitHub/DataSynthesisForCropVariety/Code/DataSynthesis/MeasureError/Gender/MeasureError/AJAE_2023/Technology-Adoption/Output/df_FM_test.csv")
+
+df <- read.csv("D:/Dropbox/BoxOld/FEDSshare/MasterGithub/CornellGitHub/DataSynthesisForCropVariety/Code/DataSynthesis/MeasureError/Gender/MeasureError/AJAE_2023/Technology-Adoption/Output/df_FM_Update.csv")
+
+results <- oaxaca(formula = selfMagIcv ~ Coop_Group_Member + Credit_Access + Extension_Access + J05_consumption_perc + ZoneID_1 + ZoneID_3 + ZoneID_4 + A03_Age + A05_Edu + Household_Size + A07_1_Main_occupatn_Salaried_employment | A02_Sex  | ZoneID_1 + ZoneID_3 + ZoneID_4, data=df, reg.fun = glm, family = binomial(link = "probit"), R=100)
+
+df_f_self <- subset(df,A02_Sex==0)
+df_m_self <- subset(df,A02_Sex==1)
